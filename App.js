e6f6f145e58665a3059026d60692bae16b3bab8a
nymph <namph2501@gmail.com>
2019-04-01 17:17:40 +0700

    test ok

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ export default class App extends React.Component {
         require('./assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
-        // This is the font that we are using for our tab bar
+        // This is the font that we are using for our tab bar dsadsa
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app  dhdksahk
