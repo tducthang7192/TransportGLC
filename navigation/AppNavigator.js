a6e4277c5cc81a69743ad29b1fcaad8e89ad6306
Mr Còi <tducthang7192@gmail.com>
2019-04-02 21:54:13 +0700

    Complete Líst

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,11 +3,10 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import Login from '../screens/Login';
-import LoginScreen1 from '../screens/Login';
 import ListsScreen from '../screens/List';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main:ListsScreen ,
+  Main: ListsScreen,
 }));
--- screens/List.js
+++ screens/List.js
@@ -8,12 +8,15 @@ import {
   Dimensions,
   ScrollView,
   SafeAreaView,
+  TouchableHighlight
+  
 } from 'react-native';
 import { Avatar, Button, Icon } from 'react-native-elements';
 
 import { cacheFonts } from '../src/helpers/AssetsCaching';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+
 
 const USERS = [
   {
@@ -52,6 +55,48 @@ const USERS = [
     value: '+ 160',
     positive: true,
   },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  },
+  {
+    name: 'Katy Friedson',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
+    value: '+ 160',
+    positive: true,
+  }
 ];
 
 export default class ListsScreen extends Component {
@@ -59,7 +104,7 @@ export default class ListsScreen extends Component {
     super(props);
 
     this.state = {
-      fontLoaded: true,
+      fontLoaded: false,
     };
   }
 
@@ -71,67 +116,10 @@ export default class ListsScreen extends Component {
       bold: require('../assets/fonts/Montserrat-Bold.ttf'),
     });
 
-    this.setState({ fontLoaded: false });
+    this.setState({ fontLoaded: true });
   }
-
-  renderValue(user) {
-    const { value, positive } = user;
-
-    if (positive) {
-      return (
-        <View
-          style={{
-            backgroundColor: 'rgba(220,230,218,1)',
-            width: 70,
-            height: 28,
-            borderRadius: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'row',
-            marginLeft: 10,
-          }}
-        >
-          <Icon name="md-arrow-dropup" type="ionicon" color="green" size={25} />
-          <Text
-            style={{
-              color: 'green',
-              fontFamily: 'regular',
-              fontSize: 13,
-              marginLeft: 5,
-            }}
-          >
-            {value}
-          </Text>
-        </View>
-      );
-    } else {
-      return (
-        <View
-          style={{
-            backgroundColor: 'rgba(244,230,224,1)',
-            width: 70,
-            height: 28,
-            borderRadius: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'row',
-            marginLeft: 10,
-          }}
-        >
-          <Icon name="md-arrow-dropdown" type="ionicon" color="red" size={25} />
-          <Text
-            style={{
-              color: 'red',
-              fontFamily: 'regular',
-              fontSize: 13,
-              marginLeft: 5,
-            }}
-          >
-            {value}
-          </Text>
-        </View>
-      );
-    }
+  _onPressButton=()=>{
+    console.log("asdja")
   }
 
   renderCard(user, index) {
@@ -139,6 +127,7 @@ export default class ListsScreen extends Component {
 
     return (
       <View
+       
         key={index}
         style={{
           height: 60,
@@ -150,6 +139,8 @@ export default class ListsScreen extends Component {
           flexDirection: 'row',
         }}
       >
+              <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+
         <View style={{ flex: 2, flexDirection: 'row', alignItems: 'center' }}>
           <View style={{ marginLeft: 15 }}>
             <Avatar
@@ -179,163 +170,82 @@ export default class ListsScreen extends Component {
             marginRight: 10,
           }}
         >
-          {this.renderValue(user)}
-          <View
-            style={{
-              backgroundColor: 'rgba(222,222,222,1)',
-              width: 35,
-              height: 28,
-              borderRadius: 5,
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginHorizontal: 10,
-            }}
-          >
-            <Icon name="md-person-add" type="ionicon" color="gray" size={20} />
+         
+          <View>
+            <Button
+              title="Mores"
+              buttonStyle={{
+                width: 50,
+                height: 28,
+                backgroundColor: 'rgba(113, 154, 112, 1)',
+                borderRadius: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginHorizontal: 10,
+              }}
+              titleStyle={{
+                fontFamily: 'regular',
+                fontSize: 8,
+                color: 'white',
+              }}
+              onPress={() => console.log('aye')}
+              underlayColor="transparent"
+            />
+          </View>
+          <View>
+            <Button
+              title="Mores"
+              buttonStyle={{
+                width: 50,
+                height: 28,
+                backgroundColor: 'rgba(113, 154, 112, 1)',
+                borderRadius: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginHorizontal: 10,
+              }}
+              titleStyle={{
+                fontFamily: 'regular',
+                fontSize: 8,
+                color: 'white',
+              }}
+              onPress={() => console.log('aye')}
+              underlayColor="transparent"
+            />
           </View>
         </View>
+        </TouchableHighlight>
+
       </View>
     );
   }
 
   renderListCards() {
     return _.map(USERS, (user, index) => {
+      console.log(user)
       return this.renderCard(user, index);
     });
   }
 
   render() {
+
     return (
       <View>
-        {this.state.fontLoaded ? (
-          <SafeAreaView
-            style={{ flex: 1, backgroundColor: 'rgba(241,240,241,1)' }}
-          >
-            <View style={styles.statusBar} />
-            <View style={styles.navBar}>
-              <Text style={styles.nameHeader}>Growing</Text>
-            </View>
-            <ScrollView style={{ flex: 1, marginBottom: 20 }}>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: 'column',
-                  backgroundColor: 'white',
-                  borderRadius: 5,
-                  alignItems: 'center',
-                  marginHorizontal: 10,
-                  height: 250,
-                  marginBottom: 10,
-                }}
-              >
-                <View style={{ flex: 3, flexDirection: 'row' }}>
-                  <View
-                    style={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <Avatar
-                      width={145}
-                      height={145}
-                      source={{
-                        uri:
-                          'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg',
-                      }}
-                      activeOpacity={0.7}
-                      avatarStyle={{ borderRadius: 145 / 2 }}
-                      overlayContainerStyle={{ backgroundColor: 'transparent' }}
-                    />
-                  </View>
-                  <View
-                    style={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <View
-                      style={{
-                        flex: 1,
-                        marginTop: 10,
-                        justifyContent: 'center',
-                      }}
-                    >
-                      <Text
-                        style={{
-                          fontFamily: 'bold',
-                          fontSize: 25,
-                          color: 'rgba(98,93,144,1)',
-                          marginLeft: -15,
-                        }}
-                      >
-                        Paul Allen
-                      </Text>
-                    </View>
-                  </View>
-                </View>
-                <View
-                  style={{
-                    width: 300,
-                    borderWidth: 0.5,
-                    borderColor: 'rgba(222, 223, 226, 1)',
-                    marginHorizontal: 20,
-                    height: 1,
-                    marginVertical: 10,
-                  }}
-                />
-                <View
-                  style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}
-                >
-                  <View style={{ flex: 1, alignItems: 'center' }}>
-                    <Button
-                      title="View Profile"
-                      buttonStyle={{
-                        height: 33,
-                        width: 120,
-                        backgroundColor: 'rgba(222, 223, 226, 1)',
-                        borderRadius: 5,
-                      }}
-                      titleStyle={{
-                        fontFamily: 'regular',
-                        fontSize: 13,
-                        color: 'gray',
-                      }}
-                      onPress={() => console.log('aye')}
-                      underlayColor="transparent"
-                    />
-                  </View>
-                  <View style={{ flex: 1, alignItems: 'center' }}>
-                    <Button
-                      title="Add User"
-                      buttonStyle={{
-                        height: 33,
-                        width: 120,
-                        backgroundColor: 'rgba(113, 154, 112, 1)',
-                        borderRadius: 5,
-                      }}
-                      titleStyle={{
-                        fontFamily: 'regular',
-                        fontSize: 13,
-                        color: 'white',
-                      }}
-                      onPress={() => console.log('aye')}
-                      underlayColor="transparent"
-                    />
-                  </View>
-                </View>
-              </View>
+        <ScrollView>
+          {this.state.fontLoaded ? (
+            <SafeAreaView
+              style={{ flex: 1, backgroundColor: 'rgba(241,240,241,1)' }}
+            >
+
+
               {this.renderListCards()}
-            </ScrollView>
-          </SafeAreaView>
-        ) : (
-          <Text>Loading...</Text>
-        )}
+
+
+            </SafeAreaView>
+          ) : (
+              <Text>Loading...</Text>
+            )}
+        </ScrollView>
       </View>
     );
   }
